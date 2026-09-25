ef6f2aefd6b4cdc3fea84664446f484261f69ec0
zydan25 <zydanalattab25w@gmail.com>
2026-09-25 19:05:45 +0300

    chore: move takhfid1 gunicorn to port 4008

--- deploy/takhfid1/ecosystem.config.cjs
+++ deploy/takhfid1/ecosystem.config.cjs
@@ -3,7 +3,7 @@ module.exports = {
     name: "takhfid1",
     cwd: "/home/root/projects/takhfid1",
     script: "/home/root/projects/takhfid1/.venv/bin/gunicorn",
-    args: "-w 3 -k gthread --threads 4 --timeout 120 --bind 127.0.0.1:4006 'app:create_app()'",
+    args: "-w 3 -k gthread --threads 4 --timeout 120 --bind 127.0.0.1:4008 'app:create_app()'",
     interpreter: "none",
     autorestart: true,
     max_restarts: 10,
