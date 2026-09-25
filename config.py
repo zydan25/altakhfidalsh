8da7810542a9854f2820494d440bf4f4a61f5660
zydan25 <zydanalattab25w@gmail.com>
2026-09-25 16:27:11 +0300

    feat: add media storage configuration

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
 import os
+from pathlib import Path
+
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "change-me")
@@ -9,3 +11,11 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAX_CONTENT_LENGTH = int(os.getenv("MAX_UPLOAD_BYTES", 25 * 1024 * 1024))
     APP_TIMEZONE = os.getenv("APP_TIMEZONE", "Asia/Aden")
+
+    MEDIA_ROOT = os.getenv(
+        "MEDIA_ROOT",
+        str(Path(__file__).resolve().parent / "storage" / "media"),
+    )
+    MEDIA_BASE_URL = os.getenv("MEDIA_BASE_URL", "/media")
+    MEDIA_MAX_SIDE = int(os.getenv("MEDIA_MAX_SIDE", "1600"))
+    MEDIA_WEBP_QUALITY = int(os.getenv("MEDIA_WEBP_QUALITY", "82"))
