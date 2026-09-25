1ba526134ffb6523fe5d4ff73eebacc69f9a8ea3
zydan25 <zydanalattab25w@gmail.com>
2026-09-25 16:25:39 +0300

    refactor: namespace modular admin APIs

--- app/modules/commerce/__init__.py
+++ app/modules/commerce/__init__.py
@@ -1,4 +1,4 @@
 from flask import Blueprint
 
-api_bp = Blueprint("commerce_module_api", __name__, url_prefix="/commerce")
+api_bp = Blueprint("commerce_module_api", __name__)
 from . import api  # noqa: E402,F401
