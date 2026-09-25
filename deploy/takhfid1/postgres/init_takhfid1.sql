9ef8a99c2a9aaaedfda6d90c07c6bcf5ef28fa5c
zydan25 <zydanalattab25w@gmail.com>
2026-09-25 18:26:27 +0300

    fix: make PostgreSQL init compatible with psql variables

--- deploy/takhfid1/postgres/init_takhfid1.sql
+++ deploy/takhfid1/postgres/init_takhfid1.sql
@@ -1,13 +1,18 @@
+-- PostgreSQL initialization for takhfid1.
 -- Run with:
 -- psql -v db_password='...' -U postgres -f init_takhfid1.sql
-DO $$
-BEGIN
-    IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'takhfid1') THEN
-        EXECUTE format('CREATE ROLE takhfid1 LOGIN PASSWORD %L', :'db_password');
-    ELSE
-        EXECUTE format('ALTER ROLE takhfid1 WITH LOGIN PASSWORD %L', :'db_password');
-    END IF;
-END $$;
+
+SELECT format('CREATE ROLE takhfid1 LOGIN PASSWORD %L', :'db_password')
+WHERE NOT EXISTS (
+    SELECT 1 FROM pg_roles WHERE rolname = 'takhfid1'
+)\gexec
+
+SELECT format('ALTER ROLE takhfid1 WITH LOGIN PASSWORD %L', :'db_password')
+WHERE EXISTS (
+    SELECT 1 FROM pg_roles WHERE rolname = 'takhfid1'
+)\gexec
 
 SELECT format('CREATE DATABASE takhfid1 OWNER takhfid1')
-WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'takhfid1')\gexec
+WHERE NOT EXISTS (
+    SELECT 1 FROM pg_database WHERE datname = 'takhfid1'
+)\gexec
