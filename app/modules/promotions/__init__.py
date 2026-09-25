5f02ec0c397f55da119c9d9ac8eb327d55c28004
zydan25 <zydanalattab25w@gmail.com>
2026-09-25 16:25:46 +0300

    refactor: namespace modular admin APIs

--- app/modules/promotions/__init__.py
+++ app/modules/promotions/__init__.py
@@ -1,4 +1,4 @@
 from flask import Blueprint
 
-api_bp = Blueprint("promotions_module_api", __name__, url_prefix="/promotions")
+api_bp = Blueprint("promotions_module_api", __name__)
 from . import api  # noqa: E402,F401
